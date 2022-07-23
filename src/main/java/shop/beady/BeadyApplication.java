cea852d0b9f2693200158828528ca0735592bf06
Tima <timathevedmed@gmail.com>
2022-07-23 23:48:24 +0300

    Upload Spring Initializr generated template

---
+++ src/main/java/shop/beady/BeadyApplication.java
@@ -0,0 +1,13 @@
+package shop.beady;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeadyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeadyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/beady/BeadyApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.beady;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeadyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
