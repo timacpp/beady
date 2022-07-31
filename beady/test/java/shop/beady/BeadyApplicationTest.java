50af00eab354ced8355d60148655b65fed8dded0
Tima <timathevedmed@gmail.com>
2022-07-31 14:31:36 +0200

    Add a dummy test

---
+++ beady/test/java/shop/beady/BeadyApplicationTest.java
@@ -0,0 +1,13 @@
+package shop.beady;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BeadyApplicationTest {
+
+	@Test
+	void dummyTest() {
+		assertTrue(true);
+	}
+}
