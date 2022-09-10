c61dee1ed787ff47ef3976c1d1cc3879a5f5dde9
Tima <timathevedmed@gmail.com>
2022-09-10 22:07:17 +0200

    Rework project structure for microservices

---
+++ microservices/feedback/src/test/java/beady/shop/feedback/FeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package beady.shop.feedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
