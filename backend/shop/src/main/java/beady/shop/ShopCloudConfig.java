4bcc734a9a34fe911ee4063d549e0d76b6298b25
Tima <timathevedmed@gmail.com>
2022-09-13 21:39:00 +0200

    Implement shop API gateway

---
+++ backend/shop/src/main/java/beady/shop/ShopCloudConfig.java
@@ -0,0 +1,19 @@
+package beady.shop;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ShopCloudConfig {
+
+	@Bean
+	public RouteLocator getRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r.path("/item/**").uri("http://localhost:8081"))
+				.route(r -> r.path("/order/**").uri("http://localhost:8082"))
+				.route(r -> r.path("/feedback/**").uri("http://localhost:8083"))
+				.build();
+	}
+}
