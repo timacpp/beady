74f6462b420e0f97aa16eb53ec61cc4ba0e4f595
Tima <timathevedmed@gmail.com>
2022-09-14 06:50:11 +0200

    Insert indices starting from 1

--- etc/sql/initial_inserts.sql
+++ etc/sql/initial_inserts.sql
@@ -1,5 +1,5 @@
-insert into item (name, price, quantity, rating) values
-	('Blue vintage necklace', 30, 3, 5),
-	('Forest choker', 15, 2, 5),
-	('Flower chokers', 10, 6, 5),
-	('Fairy necklace', 25, 1, 5);
+insert into item (id, name, price, quantity, rating) values
+	(1, 'Blue vintage necklace', 30, 3, 5),
+	(2, 'Forest choker', 15, 2, 5),
+	(3, 'Flower chokers', 10, 6, 5),
+	(4, 'Fairy necklace', 25, 1, 5);
