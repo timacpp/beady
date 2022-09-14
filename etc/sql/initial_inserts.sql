75f128ae8ac286b86ed128e834cddecd640f0c94
Tima <timathevedmed@gmail.com>
2022-09-14 02:21:23 +0200

    Change initial item rating

--- etc/sql/initial_inserts.sql
+++ etc/sql/initial_inserts.sql
@@ -1,5 +1,5 @@
 insert into item (name, price, quantity, rating) values
-	('Blue vintage necklace', 30, 3, 0),
-	('Forest choker', 15, 2, 0),
-	('Flower chokers', 10, 6, 0),
-	('Fairy necklace', 25, 1, 0);
+	('Blue vintage necklace', 30, 3, 5),
+	('Forest choker', 15, 2, 5),
+	('Flower chokers', 10, 6, 5),
+	('Fairy necklace', 25, 1, 5);
